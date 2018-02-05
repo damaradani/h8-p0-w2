0412f11ed816360267f8b1d544c7ddf2dbf95df6
Dani Damara <danny12march@gmail.com>
2018-02-05 19:38:23 +0700

    Update Exercise13-w2.js
    Menambahkan coding untuk function tanpa menggunakan match

--- jumat week2/Exercise13-w2.js
+++ jumat week2/Exercise13-w2.js
@@ -16,3 +16,29 @@ console.log(xo('oxooxo'));
 console.log(xo('oxo'));
 console.log(xo('xxxooo'));
 console.log(xo('xoxooxxo'));
+
+//Tanpa menggunakan match function
+function xo2(str) {
+  var x = 0;
+  var o = 0;
+  var kurang = 1;
+  var hasil ='';
+  for(var i = str.length; i >0 ; i--){
+    hasil = str[str.length - kurang];
+    if (hasil == 'x'){
+      x += 1;
+    }else{
+      o += 1;
+    }
+    kurang++;
+    //console.log(hasil);
+  }
+  return x === o;
+}
+
+// TEST CASES
+console.log(xo2('xoxoxo')); // true
+console.log(xo2('oxooxo')); // false
+console.log(xo2('oxo')); // false
+console.log(xo2('xxxooo')); // true
+console.log(xo2('xoxooxxo')); // true
